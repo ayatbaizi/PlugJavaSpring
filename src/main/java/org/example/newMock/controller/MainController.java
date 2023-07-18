cdc82a3eacbd4ca52e306ce02e35e2a3e5dae88b
ayatbaizi <ayatbaizi@gmail.com>
2023-07-19 03:42:53 +0600

    Added generatorRandomBalance method, and currency

--- src/main/java/org/example/newMock/controller/MainController.java
+++ src/main/java/org/example/newMock/controller/MainController.java
@@ -15,14 +15,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
 
 @RestController
 public class MainController {
 
     private Logger log = LoggerFactory.getLogger(MainController.class);
-
+    Random random = new Random();
     ObjectMapper mapper = new ObjectMapper();
+    public long startTime = 0L;
 
+    private static String generateRandomBalance(double bound) {
+       /* Random random = new Random();
+        return String.valueOf(random.nextDouble(bound) + 1);*/
+        Random random = new Random();
+        double value = random.nextDouble() * bound;
+        return String.valueOf(new BigDecimal(value).setScale(2, BigDecimal.ROUND_HALF_UP));
+    }
     @PostMapping(
             value = "/info/postBalances",
             produces = MediaType.APPLICATION_JSON_VALUE,
@@ -34,13 +44,22 @@ public class MainController {
             String clienId = requestDTO.getClientId();
             char firstDigit = clienId.charAt(0);
             BigDecimal maxLimit;
+            String currency1;
+            double bound1 = 0;
+            //int randomValueBalance = random.nextDouble(bound) + 1;
 
             if (firstDigit == '8') {
                 maxLimit = new BigDecimal(2000.00);
+                currency1 = "US";
+                bound1 = 2000.00;
             } else if (firstDigit == '9') {
                 maxLimit = new BigDecimal(1000.00);
+                currency1 = "EU";
+                bound1 = 1000.00;
             } else {
                 maxLimit = new BigDecimal(10000.00);
+                currency1 = "RUB";
+                bound1 = 10000.00;
             }
 
             String RqUID = requestDTO.getRqUID();
@@ -50,18 +69,23 @@ public class MainController {
             responseDTO.setRqUID(RqUID);
             responseDTO.setClientId(clienId);
             responseDTO.setAccount(requestDTO.getAccount());
-            responseDTO.setCurrency("RUB");
-            responseDTO.setBalance("900");
+            responseDTO.setCurrency(currency1);
+            responseDTO.setBalance(generateRandomBalance(bound1));
             responseDTO.setMaxLimit(maxLimit);
 
             log.error("***** Запрос *****" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(requestDTO));
             log.error("***** Запрос *****" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(responseDTO));
+
+            long pacing = ThreadLocalRandom.current().nextLong(100, 500);
+            long endTime = System.currentTimeMillis();
+            if(endTime - startTime < pacing)
+                Thread.sleep(pacing - (endTime - startTime));
+
             return responseDTO;
 
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
-
     }
 
     @GetMapping(
@@ -73,13 +97,21 @@ public class MainController {
             String clienId = requestDTO.getClientId();
             char firstDigit = clienId.charAt(0);
             BigDecimal maxLimit;
+            String currency2;
+            double bound2 = 0;
 
             if (firstDigit == '8') {
                 maxLimit = new BigDecimal(2000.00);
+                currency2 = "US";
+                bound2 = 2000.00;
             } else if (firstDigit == '9') {
                 maxLimit = new BigDecimal(1000.00);
+                currency2 = "EU";
+                bound2 = 1000.00;
             } else {
                 maxLimit = new BigDecimal(10000.00);
+                currency2 = "RUB";
+                bound2 = 10000.00;
             }
 
             String RqUID = requestDTO.getRqUID();
@@ -89,8 +121,8 @@ public class MainController {
             responseDTO.setRqUID(RqUID);
             responseDTO.setClientId(clienId);
             responseDTO.setAccount(requestDTO.getAccount());
-            responseDTO.setCurrency("RUB");
-            responseDTO.setBalance("900");
+            responseDTO.setCurrency(currency2);
+            responseDTO.setBalance(generateRandomBalance(bound2));
             responseDTO.setMaxLimit(maxLimit);
 
             log.error("***** Запрос *****" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(requestDTO));
